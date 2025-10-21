3de1cb8ba0fef8aad9853daff8115d77b3bebe9b
romandrabyniuk <romandrabyniuk@gmail.com>
2025-10-21 23:12:23 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
+/* global IntersectionObserver */
 'use strict';
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+}, {});
+
+const categoriesElements = document.querySelectorAll(
+  '.categories,.recommended',
+);
+
+categoriesElements.forEach((el) => observer.observe(el));
